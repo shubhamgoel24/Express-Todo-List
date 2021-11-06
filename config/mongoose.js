0fc5f703b13ed33cbcd18c8cf7a4bda4cd129853
Shubham Goel <sshubhamgoel24@gmail.com>
2021-11-06 11:20:46 +0530

    Initial

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://admin:admin123@cluster0.80coc.mongodb.net/todo_task_db?retryWrites=true&w=majority');
+mongoose.connect(process.env.db || 'mongodb://localhost/todo_task_db');
 const db = mongoose.connection;
 db.on('error',console.error.bind(console,'Error Connecting Database'));
 db.once('open',function(){
