e782c741a2e5dfdedf987cfca6bcfce4c9b2f3d9
Shubham Goel <sshubhamgoel24@gmail.com>
2021-11-06 11:11:14 +0530

    Heroku db

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/todo_task_db');
+mongoose.connect('mongodb+srv://admin:admin123@cluster0.80coc.mongodb.net/todo_task_db?retryWrites=true&w=majority');
 const db = mongoose.connection;
 db.on('error',console.error.bind(console,'Error Connecting Database'));
 db.once('open',function(){
